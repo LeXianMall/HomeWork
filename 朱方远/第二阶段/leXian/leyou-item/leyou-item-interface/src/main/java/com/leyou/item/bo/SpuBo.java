0f2539ae1d6a1c704650201834d0f0713c1aa641
小朱 <46770241+dongzhishamate@users.noreply.github.com>
2020-02-07 18:44:40 +0800

    Add files via upload

---
+++ 朱方远/第二阶段/leXian/leyou-item/leyou-item-interface/src/main/java/com/leyou/item/bo/SpuBo.java
@@ -0,0 +1,24 @@
+package com.leyou.item.bo;
+
+import com.leyou.item.pojo.Spu;
+
+public class SpuBo extends Spu {
+    String cname;
+    String bname;
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getBname() {
+        return bname;
+    }
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
+}
